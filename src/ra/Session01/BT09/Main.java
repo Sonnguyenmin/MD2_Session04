98d7ff12927e9bcba108ae4abec1e927cb915c25
Son Nguyen <nguyensoncl184@email.com>
2024-06-08 22:04:22 +0700

    Bai09

--- src/ra/Session01/BT09/Main.java
+++ src/ra/Session01/BT09/Main.java
@@ -3,34 +3,44 @@ package ra.Session01.BT09;
 import java.util.Scanner;
 
 public class Main {
+    //MAng luu cac doi tuong sinh vien dang quan ly
+    public static Students[] arrStudents = new Students[100];
+    //Chi so phan tu nho nhat trong mang chua luu sinh vien
+    public static int currentIndex = 0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-         final int MAX_STUDENTS = 10;
-         Students[] students = new Students[MAX_STUDENTS];
-         int numStudents = 0;
         do{
             System.out.println("**********MENU**************");
-            System.out.println("1. Nhap thong tin danh sach hoc sinh");
-            System.out.println("2. Hien thi thong tin hoc sinh");
-            System.out.println("3. Them moi thong tin hoc sinh");
-            System.out.println("4. Sua thong tin dua vao ma hoc sinh");
-            System.out.println("5. Xoa Hoc sinh theo Id");
-            System.out.println("6. Thoat");
+            System.out.println("1. Hien thi thong tin hoc sinh");
+            System.out.println("2. Them moi thong tin hoc sinh");
+            System.out.println("3. Sua thong tin dua vao ma hoc sinh");
+            System.out.println("4. Xoa Hoc sinh theo Id");
+            System.out.println("5. Tim sinh vien theo ten sinh vien");
+            System.out.println("6. Thong ke sinh vien theo khoang tuoi");
+            System.out.println("7. Sap xep sinh vien theo tuoi tang dan");
+            System.out.println("8. Thoat");
             System.out.println("Moi ban nhap cac so tu 1 den 5: ");
             int choie = Integer.parseInt(scanner.nextLine());
             switch (choie) {
                 case 1:
-
+                    displayListStudent();
                     break;
                 case 2:
+                    inputListstudent(scanner);
                     break;
                 case 3:
+                    updateStudent(scanner);
                     break;
                 case 4:
+                    deleteStudent(scanner);
                     break;
                 case 5:
+                    statiticStudent(scanner);
                     break;
-                case 6:
+                case 6 :
+                    sortStudent();
+                    break;
+                case 7:
                     System.out.println("Ban da ket thuc chuong trinh");
                     System.exit(0);
                     break;
@@ -40,4 +50,126 @@ public class Main {
             }
         }while (true);
     }
+    public static void displayListStudent() {
+        for (int i = 0; i < currentIndex; i++) {
+            arrStudents[i].displayData();
+        }
+    }
+
+    public static void inputListstudent(Scanner scanner) {
+        System.out.println("Nhập số sinh viên cần nhập thông tin:");
+        int countStudent = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < countStudent; i++) {
+            //Nhập thông tin cho sinh viên có chỉ số currentIndex trong mảng
+            arrStudents[currentIndex] = new Students();
+            arrStudents[currentIndex].inputData(scanner);
+            currentIndex++;
+        }
+    }
+
+    public static void updateStudent(Scanner scanner) {
+        System.out.println("Nhập vào mã sinh viên cần cập nhật:");
+        String studentId = scanner.nextLine();
+        int indexUpdate = getIndexById(studentId);
+        if (indexUpdate >= 0) {
+            //Thực hiện cập nhật
+            boolean isExit = true;
+            do {
+                System.out.println("1. Cập nhật tên sinh viên");
+                System.out.println("2. Cập nhật tuổi sinh viên");
+                System.out.println("3. Cập nhật giới tính sinh viên");
+                System.out.println("4. Cập nhật địa chỉ sinh viên");
+                System.out.println("5. Cập nhật số điện thoại sinh viên");
+                System.out.println("6. Thoát");
+                System.out.print("Lựa chọn của bạn:");
+                int choice = Integer.parseInt(scanner.nextLine());
+                switch (choice) {
+                    case 1:
+                        arrStudents[indexUpdate].setStudentName(scanner.nextLine());
+                        break;
+                    case 2:
+                        arrStudents[indexUpdate].setAge(Integer.parseInt(scanner.nextLine()));
+                        break;
+                    case 3:
+                        arrStudents[indexUpdate].setGender(Boolean.parseBoolean(scanner.nextLine()));
+                        break;
+                    case 4:
+                        arrStudents[indexUpdate].setAddress(scanner.nextLine());
+                        break;
+                    case 5:
+                        arrStudents[indexUpdate].setPhone(scanner.nextLine());
+                        break;
+                    default:
+                        isExit = false;
+                }
+            } while (isExit);
+        } else {
+            System.err.println("Mã sinh viên không tồn tại");
+        }
+    }
+
+    public static int getIndexById(String studentId) {
+        for (int i = 0; i < currentIndex; i++) {
+            if (arrStudents[i].getStudentId().equals(studentId)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void deleteStudent(Scanner scanner) {
+        System.out.println("Nhập vào mã sinh viên cần xóa:");
+        String studentId = scanner.nextLine();
+        int indexDelete = getIndexById(studentId);
+        if (indexDelete >= 0) {
+            for (int i = indexDelete; i < currentIndex; i++) {
+                arrStudents[i] = arrStudents[i + 1];
+            }
+            currentIndex--;
+        } else {
+            System.err.println("Mã sinh viên không tồn tại");
+        }
+    }
+
+    public static void searchStudentByName (Scanner scanner) {
+        System.out.println("Nhập vào tên sinh viên cần tìm:");
+        String studentNameSeach = scanner.nextLine();
+        int cntStudent = 0;
+        System.out.println("Các sinh viên tìm thấy:");
+        for (int i = 0; i < currentIndex; i++) {
+            if (arrStudents[i].getStudentName().toLowerCase().contains(studentNameSeach.toLowerCase())) {
+                arrStudents[i].displayData();
+                cntStudent++;
+            }
+        }
+        System.out.printf("Có %d sinh viên được tìm thấy\n", cntStudent);
+    }
+
+    public static void statiticStudent(Scanner scanner) {
+        System.out.println("Nhập vào tuổi bắt đầu:");
+        int fromAge = Integer.parseInt(scanner.nextLine());
+        System.out.println("Nhập vào tuổi kết thúc:");
+        int toAge = Integer.parseInt(scanner.nextLine());
+        int countStudent = 0;
+        for (int i = 0; i < currentIndex; i++) {
+            if(arrStudents[i].getAge() >= fromAge && arrStudents[i].getAge() <= toAge) {
+                countStudent++;
+            }
+        }
+        System.out.printf("Có %d sinh viên có tuổi từ %d đến %d\n", countStudent, fromAge, toAge);
+    }
+
+    public static void sortStudent() {
+        for (int i = 0; i < currentIndex - 1; i++) {
+            for (int j = i + 1; j < currentIndex; j++) {
+                if (arrStudents[i].getAge() > arrStudents[j].getAge()) {
+                    Students temp = arrStudents[i];
+                    arrStudents[i] = arrStudents[j];
+                    arrStudents[j] = temp;
+                }
+            }
+        }
+        System.out.println("Đã sắp xếp xong sinh viên theo tuổi tăng dần");
+    }
+
 }
--- src/ra/Session01/BT09/Students.java
+++ src/ra/Session01/BT09/Students.java
@@ -3,13 +3,17 @@ package ra.Session01.BT09;
 import java.util.Scanner;
 
 public class Students {
-    private String studentId;
+
+    //1. Fields/ Attributes/ Properties
+
+    private String studentId; //Tinh bao dong
     private String studentName;
     private int age;
     private boolean gender;
     private String address;
     private String phone;
 
+    //2. Constructors
 
     public Students() {
     }
@@ -22,6 +26,9 @@ public class Students {
         this.address = address;
         this.phone = phone;
     }
+
+    //3. Method
+    //-3.1. getter/ Setter
 
     public String getStudentId() {
         return studentId;
@@ -70,31 +77,149 @@ public class Students {
     public void setPhone(String phone) {
         this.phone = phone;
     }
-
-    public void inputStudent(Scanner scanner) {
-        System.out.print("Enter student ID: ");
-        studentId = scanner.nextLine();
+    //-3.2. Method behavior
+    public void inputData(Scanner scanner) {
+        //1. Nhap ma sinh vien: Gom 5 ky tu bat dau la sv, khoong strung lap
+//        System.out.print("Enter student ID: ");
+//        studentId = scanner.nextLine();
+        this.studentId = inputStudentId(scanner);
+        //2. Nha ten sinh vien: Phai co tu 6 den 50 ky tu
         System.out.print("Enter student name: ");
         studentName = scanner.nextLine();
+        //3. Tuoi sinh vien phai co gia tri lon hon hoac bang 18
         System.out.print("Enter student age: ");
         age = Integer.parseInt(scanner.nextLine());
+        //4. Gioi tinh chi nhan true/ false
         System.out.print("Enter student gender: ");
         gender = Boolean.parseBoolean(scanner.nextLine());
+        //5. Dia chi: Khong duoc rong
         System.out.print("Enter student address: ");
         address = scanner.nextLine();
+        //6. Dien thoai: Gom 10 ky tu bat dau bang ky tu 0
         System.out.print("Enter student phone: ");
         phone = scanner.nextLine();
     }
 
-    @Override
-    public String toString() {
-        return "Students{" +
-                "studentId='" + this.studentId + '\'' +
-                ", studentName='" + this.studentName + '\'' +
-                ", age=" + this.age +
-                ", gender=" + this.gender +
-                ", address='" + this.address + '\'' +
-                ", phone='" + this.phone + '\'' +
-                '}';
+    public String inputStudentId(Scanner scanner ) {
+        System.out.print("Enter student ID: ");
+        do {
+            String studentId = scanner.nextLine();
+            if(studentId.length() == 5) {
+                if(studentId.startsWith("SV")) {
+//                    return studentId;
+                        //Khong trung lap
+                    boolean isExit = false; //chua ton tai
+                    for (Students student: Main.arrStudents) {
+                        if(student.getStudentId().equals(studentId)) {
+                            isExit = true;//da ton tai
+                            break;
+                        }
+                    }
+                    if(isExit) {
+                        System.err.println("Ma sinh vien da ton tai, Vui long nhap lai");
+                    }
+                    else  {
+                        return studentId;
+                    }
+                }
+                else {
+                    System.err.println("Ma sinh vien bat dau la sv, vui long nhap lai");
+                }
+            }
+            else  {
+                System.err.println("Ma sinh vien gom 5 ky tu, vui long nhap lai");
+            }
+        }
+        while (true);
+    }
+
+    public String inputStudentName(Scanner scanner) {
+        System.out.println("Nhap vao ten sinh vien: ");
+        do {
+            String studentName = scanner.nextLine();
+            if(studentName.length() >= 6 && studentName.length() <= 50) {
+                return studentName;
+            }
+            else  {
+                System.err.println("Ten sinh vien tu 6 - 50 ky tu, vui long nhap lai");
+            }
+        }
+        while (true);
+    }
+
+    public int inputAge(Scanner scanner) {
+        System.out.println("Nhap vao tuoi sinh vien: ");
+        do {
+             int age = Integer.parseInt(scanner.nextLine());
+             if(age>= 18) {
+                 return age;
+             }
+             else  {
+                 System.err.println("Sinh vien phai co do tuoi tu 18 tro len, vui long nhap lai");
+             }
+        }
+        while (true);
+    }
+
+    public boolean inputGender(Scanner scanner) {
+        System.out.println("Nhap vao gioi tinh sinh vien: ");
+        do {
+            String gender = scanner.nextLine();
+            if(gender.equals("true") || gender.equals("false")) {
+                return Boolean.parseBoolean(gender);
+            }
+            else  {
+                System.err.println("Gioi tinh sinh vien chi nhan true | false, Vui long nhap lai");
+            }
+
+        }
+        while (true);
+    }
+
+    public String inputAddress(Scanner scanner) {
+        System.out.println("Nhap vao dia chi sinh vien: ");
+        do {
+            String address = scanner.nextLine();
+            if (address.trim().isEmpty()) {
+                System.err.println("CHua nhap dia chi, vui long nhap lai");
+            }
+            else  {
+                return address;
+            }
+        }
+        while (true);
+    }
+
+    public String inputPhone(Scanner scanner) {
+        System.out.println("Nhap vao So dien thoai sinh vien: ");
+        do {
+            String phone = scanner.nextLine();
+            if( phone.charAt(0)== '0') {
+                if(phone.length() == 10) {
+                    boolean isExit = false;
+                    for (Students student : Main.arrStudents) {
+//                        if( ) {
+//
+//                        }
+//                        else {
+//                            return phone;
+//                        }
+                    }
+                }
+                else  {
+                    System.err.println("So dien thoai co 10 ky tu, vui long nhap lai");
+                }
+            }
+            else  {
+                System.out.println("So dien thoai bat dau la 0, vui long nhap lai");
+            }
+        }
+        while (true);
+    }
+
+    public void displayData() {
+        System.out.printf("Mã SV: %s - Tên SV: %s - Tuổi: %d - Giới tính: %s\n",
+                this.studentId, this.studentName, this.age, this.gender ? "Nam" : "Nữ");
+        System.out.printf("Địa chỉ: %s - Số điện thoại: %s\n", this.address, this.phone);
     }
 }
